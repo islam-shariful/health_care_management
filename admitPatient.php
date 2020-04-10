76783f7d580430a342fed023c8a80a463b130dec
unknown <imdshariful171@gmail.com>
2020-04-11 01:31:42 +0600

    input validation in php/js

--- admitPatient.php
+++ admitPatient.php
@@ -1,65 +1,28 @@
 <?php
-	
 	$servername = "localhost";
-	$dbusername = "root";
-	$password = "";
-	$dbname = "bhc";
+						$dbusername = "root";
+						$password = "";
+						$dbname = "bhc";
+						
+
+						// Create connection
+						$conn = new mysqli($servername, $dbusername, $password, $dbname);
+						// Check connection
+						if ($conn->connect_error) {
+						    die("Connection failed: " . $conn->connect_error);
+						}
+
 	
 
-	// Create connection
-	$conn = new mysqli($servername, $dbusername, $password, $dbname);
-	// Check connection
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
-
-	/*
-					// Create connection
-					$conn = new mysqli($servername, $dbusername, $password, $dbname);
-					// Check connection
-					if ($conn->connect_error) {
-					    die("Connection failed: " . $conn->connect_error);
-					}
-
-					$sql = "Select * from admit";
-					$result = query($sql);
-
-	*/
 
 
-	//Resistration
-    if ($_SERVER["REQUEST_METHOD"] == "POST")
-	 {
-	    $pUserName = $_POST['userName'];
-	    $admitID = $_POST['admitID'];
-	    $catagory = $_POST['catagory'];
-	    $room = $_POST['room'];
-	    $date = $_POST['date'];
 
-		$sql2 = "SELECT SUM(room) AS sumRoom FROM admit WHERE catagory = '$catagory' ";
-		$res = mysqli_query($conn, $sql2);
-		$data = mysqli_fetch_array($res);
-		$sumRoom = $data['sumRoom'];
-		//echo $sumRoom;
+	
+	
 
-        $sql = "INSERT INTO `admit`(`pUserName`,`admitID`, `catagory`, `room`, `date`) VALUES ('$pUserName','$admitID','$catagory','$room','$date');" ;
-        if($sumRoom<"8") {
-
-			if ($conn->query($sql) === TRUE) {
-				$sql3 = "  INSERT INTO `bill`(`admitID`, `billAmount`) VALUES ('$admitID','0.0'); ";
-				$res2 = mysqli_query($conn, $sql3);
-			    echo "Patient Admitted successfully";
-			}else {
-			    //echo "Error: " . $sql . "<br>" . $conn->error;
-			    echo "Invalid Info";
-			}
-		}
-		else {echo "Room Full";}
-    }
-
-    $conn->close();
+	
     
-
+    
 ?>
 
 <!DOCTYPE html>
@@ -68,7 +31,10 @@
 	<title>Admit Patient | BANGABANDHU HEALTH CARE</title>
 	<link rel="stylesheet" href="css/patientManagement.css">
 	<link rel="shortcut icon" href="images/adminicon5.JPG">
+
+	
 </head>
+
 <body>
 	<!-- ------------------------------------------------------------------------------------- -->
 		<!-- Add Patient Icon -->
@@ -96,27 +62,28 @@
 			<a href="patientFind.php"><img class="spi" src="images/searchicon.png" alt="Search Patient Icon"></a>
 		</div>
 		<!-- ---------------------------------------------------------------------------- -->
-	<div class="register">
+
+		<div class="register">
 
 			<h2>ADMIT PATIENT</h2><br>
-			<form class="form" action="" method="post">
-			  <!-- User Id -->
-			  <label for="userName">User ID:</label>
-			  <input class="formBox" type="text" id="userName" name="userName" placeholder=" Enter User ID"><br><br>
-			  <!-- Admit Id -->
-			  <label for="admitID">Admit ID:</label>
-			  <input class="formBox" type="text" id="admitID" name="admitID" placeholder=" Enter Admit ID"><br><br>
 
-			  <!-- User Id --><!--
-			  <label for="pUserName">Select User :</label>
-			  <select name="pUserName">
-			  	-->
+			<!-- FORM Start -->
+			<form class="form" action="" method="post" name="myform" >  <!-- $$$$$$$$$$$$$$$$$$$$$$$$$$ -->
+			<!-- User Name -->
+			<label id="userName" for="userName">User Name:</label>
+			<input class="formBox" type="text" id="userName" name="userName" placeholder=" Enter User Name"><br><br>
+			<!-- Admit Id -->
+			<label id="admitID" for="admitID">Admit ID:</label>
+			<input class="formBox" type="text" id="admitID" name="admitID" placeholder=" Enter Admit ID"><br><br>
 
+			<!-- User Id --><!--
+			<label for="pUserName">Select User :</label>
+			<select name="pUserName">
+			-->
 
-
-
-			  <label for="catagory">Catagory :</label>
-			  <div>
+			<!-- Catagory -->
+			<label id="catagory" for="catagory">Catagory :</label>
+			<div>
 				  <input type="radio" id="premier1" name="catagory" value="premier1">
 				  <label for="premier1">Premier-Single Bed</label><br>
 				  <input type="radio" id="premier2" name="catagory" value="premier2">
@@ -126,24 +93,96 @@
 				  <label for="general1">General-Single Bed</label><br>
 				  <input type="radio" id="general2" name="catagory" value="general2">
 				  <label for="general2">General-Double Bed</label><br><br>
-			  </div>
-
-			  <div>
-			  	<label for="room">Choose Amount of Rooms:</label>
+			</div>
+			<!-- Room -->
+			<div>
+			  	<label id="room" for="room">Choose Amount of Rooms:</label>
 				<select name="room">
 				  <option value="1">1</option>
 				  <option value="2">2</option>
 				  <option value="3">3</option>
 				  <option value="4">4</option>
 				</select><br><br>
-			  </div>
-			  <!-- Admitted Date -->
-			  <label for="date">Date:</label>
- 			  <input class="formBox" type="date" id="date" name="date"><br><br>
+			</div>
+			<!-- Admitted Date -->
+			<label id="date" for="date">Date:</label>
+ 			<input class="formBox" type="date" id="date" name="date"><br><br>
 			
-			 <input style="background-color:white;" type="submit" value="ADMIT" name="resistration">
-			</form>
+			<input style="background-color:white;" onclick="CheckForBlank()" type="submit" value="ADMIT" name="resistration">
+			 
+			</form>  <!-- EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE -->
+			<!-- FORM End -->
 		</div>
+		<script type="text/javascript">
+			/*function validation() {
+				var userName = document.forms["myform"]["userName"].value;
+				var admitID = document.forms["myform"]["admitID"].value;
+				var catagory = document.forms["myform"]["catagory"].value;
+				var room = document.forms["myform"]["room"].value;
+				var date = document.forms["myform"]["date"].value;
+				//alert(room);
+			}*/
+			//check blank
+			function CheckForBlank() {
+				var userName = document.forms["myform"]["userName"].value;
+				var admitID = document.forms["myform"]["admitID"].value;
+				var catagory = document.forms["myform"]["catagory"].value;
+				var room = document.forms["myform"]["room"].value;
+				var date = document.forms["myform"]["date"].value;
+
+				var format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+				if( ((userName.trim() == "") || (admitID.trim() == "") || (catagory == "") || (room == "") || (date == "")) && (format.test(userName) || format.test(admitID)) ) {
+				  alert("No blank values & special character are allowed");
+				}
+                else if( (userName.trim() == "") || (admitID.trim() == "") || (catagory == "") || (room == "") || (date == "") ) {
+                    alert("No blank values are allowed");
+                }
+                else if( format.test(userName) || format.test(admitID) ) {
+				  alert("No special character are allowed");
+				} 
+                else {
+                	alert(" <?php 
+                		//Resistration
+					    if ($_SERVER["REQUEST_METHOD"] == "POST")
+						 {
+
+						    $pUserName = $_POST['userName'];
+						    $admitID = $_POST['admitID'];
+						    $catagory = $_POST['catagory'];
+						    $room = $_POST['room'];
+						    $date = $_POST['date'];
+
+							$sql2 = "SELECT SUM(room) AS sumRoom FROM admit WHERE catagory = '$catagory' ";
+							$res = mysqli_query($conn, $sql2);
+							$data = mysqli_fetch_array($res);
+							$sumRoom = $data['sumRoom'];
+							//echo $sumRoom;
+							//echo "no error";
+							
+					        $sql = "INSERT INTO `admit`(`pUserName`,`admitID`, `catagory`, `room`, `date`) VALUES ('$pUserName','$admitID','$catagory','$room','$date');" ;
+					        if($sumRoom<"8") {
+
+								if ($conn->query($sql) === TRUE) {
+									$sql3 = "  INSERT INTO `bill`(`admitID`, `billAmount`) VALUES ('$admitID','0.0'); ";
+									$res2 = mysqli_query($conn, $sql3);
+								    echo " Patient Admitted successfully";
+								}else {
+								    //echo "Error: " . $sql . "<br>" . $conn->error;
+								    echo " Invalid Info";
+								}
+							}
+							else {echo " Room Full";}
+					    }
+
+					    $conn->close(); ?> ");
+                	
+                }
+            }
+
+		</script>
+
+		
 
 		<!-- Back -->
 		<div>
--- patientManagement.php
+++ patientManagement.php
@@ -1,36 +1,118 @@
 <?php
 	
 	$servername = "localhost";
-	$username = "root";
-	$password = "";
+	$dbusername = "root";
+	$dbpassword = "";
 	$dbname = "bhc";
 	
-
 	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
+	$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
 	// Check connection
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
 	}
+	$error_msg['pNameError'] = $error_msg['pUserNameError'] = $error_msg['pAgeError'] = $error_msg['pAddressError'] = $error_msg['pPhoneNoError'] = $error_msg['pBirthdayError'] = $error_msg['pGenderError'] = "*";
+	$pName = $pUserName = $pAge = $pAddress = $pPhoneNo = $pBirthday = $pGender = "";
 	//Resistration
     if ($_SERVER["REQUEST_METHOD"] == "POST")
-	 {
-        $pName = $_POST['fullName'];
-	    $pUserName = $_POST['userName'];
-	    $pAge = $_POST['age'];
-	    $pAddress = $_POST['address'];
-	    $pPhoneNo = $_POST['phoneNo'];
-	    $pBirthday = $_POST['birthday'];
-	    $pGender = $_POST['gender'];
-
-        $sql = "INSERT INTO `patient`(`pName`, `pUserName`, `pAge`, `pAddress`, `pPhoneNo`, `pBirthday`, `pGender`) VALUES ('$pName','$pUserName','$pAge','$pAddress','$pPhoneNo','$pBirthday','$pGender');";
-
-		if ($conn->query($sql) === TRUE) {
-		    echo "New record created successfully";
-		} else {
-		    //echo "Error: " . $sql . "<br>" . $conn->error;
-		    //echo "Invalid Info";
+	{
+	 	function test_input($data) {
+		  $data = trim($data);
+		  $data = stripslashes($data);
+		  $data = htmlspecialchars($data);
+		  return $data;
 		}
+		//$pNameError = "Must be filled";
+		//empty check
+
+		//Full Name empty & special char check
+		if (empty($_POST["fullName"])) {
+			    $error_msg['pNameError'] = "* Full Name is required";
+		} 
+		else if (!preg_match("/^[a-zA-Z ]*$/",$_POST["fullName"])) {
+		        $error_msg['pNameError'] = "* Only letters and white space allowed";
+		}
+		else {
+			$pName = test_input($_POST["fullName"]);
+		}
+		//Username empty check
+		if (empty($_POST["userName"])) {
+			    $error_msg['pUserNameError'] = "* User Name is required";
+		} else {
+			$pUserName = test_input($_POST["userName"]);
+		}
+		//Age empty and number check
+		if (empty($_POST["age"])) {
+			$error_msg['pAgeError'] = "* Age is required";
+		}
+		else if ( !is_numeric($_POST["age"]) ) {
+		        $error_msg['pAgeError'] = "* Only digits are allowed";
+		}
+		else {
+			$pAge = test_input($_POST["age"]);
+		}
+		//Address empty check
+		if (empty($_POST["address"])) {
+			$error_msg['pAddressError'] = "* Address is required";
+		} else {
+			$pAddress = test_input($_POST["address"]);
+		}
+		//phone No empty and number check
+		if (empty($_POST["phoneNo"])) {
+			$error_msg['pPhoneNoError'] = "* Phone No is required";
+		}
+		else if ( !is_numeric($_POST["phoneNo"]) ) {
+		        $error_msg['pPhoneNoError'] = "* Only digits are allowed";
+		}
+		else {
+			$pPhoneNo = test_input($_POST["phoneNo"]);
+			
+		}
+		//Birthday
+		if (empty($_POST["birthday"])) {
+			$error_msg['pBirthdayError'] = "* Birthday is required";
+		} else {
+			$pBirthday = test_input($_POST["birthday"]);
+		}
+		//Gender
+		if (empty($_POST["gender"])) {
+			$error_msg['pGenderError'] = "* Gender is required";
+		} else {
+			$pGender = test_input($_POST["gender"]);
+		}
+
+
+		if ( $error_msg['pNameError'] != "*" || $error_msg['pUserNameError'] !="*" || $error_msg['pAgeError'] != "*" || $error_msg['pAddressError'] != "*" || $error_msg['pPhoneNoError'] != "*" || $error_msg['pBirthdayError'] != "*" || $error_msg['pGenderError'] != "*" ) {
+			echo "Invalid Input";
+	    //die();
+		 } 
+		 else 
+		 {
+		 	$sql = "INSERT INTO `patient`(`pName`, `pUserName`, `pAge`, `pAddress`, `pPhoneNo`, `pBirthday`, `pGender`) VALUES ('$pName','$pUserName','$pAge','$pAddress','$pPhoneNo','$pBirthday','$pGender');";
+
+			if ($conn->query($sql) === TRUE) {
+			    echo "New record created successfully";
+			} else {
+			    //echo "Error: " . $sql . "<br>" . $conn->error;
+			    //echo "Invalid Info";
+			}
+		  }
+
+		/*
+	    $ = test_input($_POST['']);
+	    $ = test_input($_POST['']);
+	    $ = test_input($_POST['']);
+	    $ = test_input($_POST['']);
+	    $ = test_input($_POST['']);
+	    $ = test_input($_POST['']);
+	    $ = test_input($_POST['']);
+		*/
+	    
+
+       
+	  
+	  
+
     }
 
     $conn->close();
@@ -75,25 +157,42 @@
 	<div class="register">
 
 			<h2>REGISTER PATIENT</h2><br>
-			<form class="form" action="" method="post">
+
+			<p><span class="error">* required field</span></p>
+
+			<form class="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 			  <!-- Name -->
 			  <label for="fullName">Name:</label>
-			  <input class="formBox" type="text" id="fullName" name="fullName" placeholder=" Enter Full Name"><br><br>
+			  <input class="formBox" type="text" id="fullName" name="fullName" placeholder=" Enter Full Name"><span style="color: #FF0000;padding-left: 195px;"> <?php echo $error_msg['pNameError'];?></span>
+  			  <br><br>
 			  <!--User Name -->
 			  <label for="userName">User Name:</label>
-			  <input class="formBox" type="text" id="userName" name="userName" placeholder=" Enter User Name"><br><br>
+			  <input class="formBox" type="text" id="userName" name="userName" placeholder=" Enter User Name">
+			  <span style="color: #FF0000;padding-left: 160px;"> <?php echo $error_msg['pUserNameError'];?></span>
+			  <br><br>
 			  <!-- Age -->
 			  <label for="age">Age:</label>
-			  <input class="formBox" type="text" id="age" name="age" placeholder=" Enter Age"><br><br>
+			  <input class="formBox" type="text" id="age" name="age" placeholder=" Enter Age">
+			  <span style="color: #FF0000;padding-left: 205px;"> <?php echo $error_msg['pAgeError'];?></span>
+			  <br><br>
 			  <!-- Address -->
 			  <label for="address">Address:</label>
-			  <input class="formBox" type="text" id="address" name="address" placeholder=" Enter Address"><br><br>
+			  <input class="formBox" type="text" id="address" name="address" placeholder=" Enter Address">
+			  <span style="color: #FF0000;padding-left: 180px;"> <?php echo $error_msg['pAddressError'];?></span>
+			  <br><br>
 			  <!-- Phone -->
 			  <label for="phoneNo">Phone No:</label>
-			  <input class="formBox" type="text" id="phoneNo" name="phoneNo" placeholder=" Enter Unique Phone No."><br><br>
+			  <input class="formBox" type="text" id="phoneNo" name="phoneNo" placeholder=" Enter Unique Phone No.">
+			  <span style="color: #FF0000;padding-left: 170px;"> <?php echo $error_msg['pPhoneNoError'];?></span>
+			  <br><br>
 			  <!-- Birthday -->
 			  <label for="birthday">Birthday:</label>
- 			  <input class="formBox" type="date" id="birthday" name="birthday"><br><br>
+ 			  <input class="formBox" type="date" id="birthday" name="birthday">
+ 			  
+ 			  <span style="color: #FF0000;padding-left: 150px;"> <?php echo $error_msg['pBirthdayError'];?></span>
+ 			  
+ 			  <br><br>
+
  			  <!-- Gender -->
  			  Gender : </br>
  			  <div>
@@ -102,7 +201,9 @@
 				  <input type="radio" id="female" name="gender" value="female">
 				  <label for="female">Female</label><br>
 				  <input type="radio" id="other" name="gender" value="other">
-				  <label for="other">Other</label><br><br>
+				  <label for="other">Other</label>
+				  <span style="color: #FF0000;padding-left: 20px;"> <?php echo $error_msg['pGenderError'];?></span>
+				  <br><br>
 				</div></br>
 
 			
